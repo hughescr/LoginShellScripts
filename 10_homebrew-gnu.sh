c43e50afc3b70568fba49e498a0058e59716b8fa
Craig R. Hughes <craig.git@rungie.com>
2014-11-24 17:07:24 -0800

    Add gnu sed homebrew

--- 10_homebrew-gnu.sh
+++ 10_homebrew-gnu.sh
@@ -1,5 +1,12 @@
 if [ -x /usr/local/bin/brew ]; then
+    # Coreutils
     export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
     export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+    # GNU tar
     export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
+
+    # GNU sed
+    export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+    export MANPATH="/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH"
 fi
