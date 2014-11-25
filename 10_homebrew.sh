71aed4b935621e9738099f5a415bf738c8970c9a
Craig R. Hughes <craig.git@rungie.com>
2014-11-24 18:21:19 -0800

    Typo in bash substitution to remove duplicate /usr/local/bin PATH entry

--- 10_homebrew.sh
+++ 10_homebrew.sh
@@ -1,5 +1,5 @@
 if [ -x /usr/local/bin/brew ]; then
     # Remove /usr/local/bin from existing PATH and move it up to the front
-    export PATH="/usr/local/sbin:/usr/local/bin:${PATH/:\/usr\/local\/bin//}"
+    export PATH="/usr/local/sbin:/usr/local/bin:${PATH/:\/usr\/local\/bin/}"
     export MANPATH="/usr/local/share/man:$MANPATH"
 fi
