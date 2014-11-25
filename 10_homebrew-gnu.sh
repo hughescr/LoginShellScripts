1f9fdebef7cf0fc45e424e0c17861271ccab3755
Craig R. Hughes <craig.git@rungie.com>
2014-11-24 17:05:39 -0800

    Add gnutar homebrew

--- 10_homebrew-gnu.sh
+++ 10_homebrew-gnu.sh
@@ -1,4 +1,5 @@
 if [ -x /usr/local/bin/brew ]; then
     export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
     export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+    export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:$PATH"
 fi
