9ee34291486ea3ccdc561c0ea8530a021604dc9d
Craig R. Hughes <craig.git@rungie.com>
2014-11-24 16:54:35 -0800

    Add homebrew; make macports optional

--- 10_macports.sh
+++ 10_macports.sh
@@ -1,2 +1,4 @@
-export MANPATH=/opt/local/share/man:$MANPATH
-export PATH=/opt/local/libexec/gnubin:/opt/local/bin:/opt/local/sbin:$PATH
+if [ -d /opt/local ]; then
+    export MANPATH=/opt/local/share/man:$MANPATH
+    export PATH=/opt/local/libexec/gnubin:/opt/local/bin:/opt/local/sbin:$PATH
+fi
