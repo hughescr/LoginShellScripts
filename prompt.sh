5e743dd56bc7f9ba33259ef59a2eaed4298e3134
Craig R. Hughes <craig.git@rungie.com>
2016-05-27 13:48:29 -0700

    Do not set iTerm2 user variable; that seems to suck badly

--- prompt.sh
+++ prompt.sh
@@ -1,3 +1,4 @@
+#!/usr/local/bin/bash
 set -o emacs
 
 # Terminal colours - must use \[ and \] to tell readline about them, so bash line editing works
@@ -15,7 +16,8 @@ yellow="\[$(tput setaf 3)\]"
 cyan="\[$(tput setaf 6)\]"
 
 function _git_prompt() {
-    local git_status="`git status -unormal 2>&1`"
+    local git_status
+    git_status="$(git status -unormal 2>&1)"
     if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
         if [[ "$git_status" =~ nothing\ to\ commit ]]; then
             local ansi="$inverse$bold$green"
@@ -27,21 +29,20 @@ function _git_prompt() {
         if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
             branch=${BASH_REMATCH[1]}
             test "$branch" != master || branch=''
-            branch="$branch `git describe --tags --dirty --always`"
+            branch="$branch $(git describe --tags --dirty --always)"
         elif [[ "$git_status" =~ HEAD\ detached\ (from|at)\ ([^[:space:]]+) ]]; then
             branch="tag:${BASH_REMATCH[2]}"
         else
             # Detached HEAD.  (branch=HEAD is a faster alternative.)
-            branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
-                echo HEAD`)"
+            branch="($(git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
+                echo HEAD))"
         fi
-        if [ -n "$(declare -F iterm2_set_user_var)" ]; then iterm2_set_user_var gitBranch "${branch}"; fi
         echo -n "${ansi}${branch}${reset} "
-    else
-        if [ -n "$(declare -F iterm2_set_user_var)" ]; then iterm2_set_user_var gitBranch ""; fi
     fi
 }
+
 function _prompt_command() {
-    PS1="`_git_prompt`""[\u@\h:\w]\$ "
+    PS1="$(_git_prompt)""[\u@\h:\w]\$ "
 }
+
 PROMPT_COMMAND="_prompt_command;$PROMPT_COMMAND"
