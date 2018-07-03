00eca3a2afec79e433c21755f655ba063b6c11c3
Craig R. Hughes <craig.git@rungie.com>
2018-07-03 09:15:13 -0700

    Better subl completion

--- 50_subl_completion.sh
+++ 50_subl_completion.sh
@@ -1,7 +1,3 @@
 #!/bin/env bash
-_subl_completions()
-{
-    COMPREPLY=($(compgen -f "${COMP_WORDS[-1]}"| grep -v -E '.sublime-workspace$'))
-}
 
-complete -F _subl_completions subl
+complete -f -X '*.sublime-workspace' subl
