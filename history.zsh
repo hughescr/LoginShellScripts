f3b70498fc9e8a2556bdb4229f3bf468f7cf0ece
Craig R. Hughes <craig.git@rungie.com>
2019-10-31 13:11:30 -0700

    History better options

--- history.zsh
+++ history.zsh
@@ -1,7 +1,7 @@
 export HISTSIZE=500000
 export SAVEHIST=$HISTSIZE
+setopt extended_history
 setopt share_history
-setopt inc_append_history
 setopt hist_ignore_space
 setopt hist_reduce_blanks
 setopt hist_ignore_all_dups
