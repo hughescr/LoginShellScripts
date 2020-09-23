77d6ae6ddd24398e738a5a02ccd041491c2a7cd6
Craig R. Hughes <craig.git@rungie.com>
2020-09-22 20:51:28 -0700

    Use vivid if available for LS_COLORS

--- dircolors.sh
+++ dircolors.sh
@@ -1,1 +1,5 @@
-dircolors 2>&1 > /dev/null && eval `dircolors "$HOME/.dircolors"`
+if [ -x /usr/local/bin/vivid ]; then
+    export LS_COLORS="$(vivid generate snazzy)"
+else
+    dircolors 2>&1 > /dev/null && eval `dircolors "$HOME/.dircolors"`
+fi
