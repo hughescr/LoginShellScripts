c8334091cc781fd5fe733892321182125cffa3e3
Craig R. Hughes <craig.git@rungie.com>
2022-06-04 14:49:47 -0700

    If all else fails, complete with ignored things

--- 50_zsh_completion.zsh
+++ 50_zsh_completion.zsh
@@ -1,4 +1,4 @@
-zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*' completer _complete _match _approximate _ignored
 zstyle ':completion:*:match:*' original only
 zstyle ':completion:*:approximate:*' max-errors 1 numeric
 
