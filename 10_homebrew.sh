37f9acba627b1f141152bf3ac6d97a7fe2eea3b2
Craig R. Hughes <craig.git@rungie.com>
2022-06-24 10:59:29 -0700

    Add "newbrew" alias

--- 10_homebrew.sh
+++ 10_homebrew.sh
@@ -3,3 +3,5 @@ if [ -x /opt/homebrew/bin/brew ]; then
 elif [ -x /usr/local/bin/brew ]; then
     eval $(/usr/local/bin/brew shellenv)
 fi
+
+alias newbrew='(for TAP in $(find ${HOMEBREW_PREFIX}/Library/Taps -type d -name .git); do git --git-dir="${TAP}" changes --no-merges --diff-filter=A --since="2 weeks ago"; done) | fgrep new | cut -d" " -f 5-6 | align -jt'
