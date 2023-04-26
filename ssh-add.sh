012fca1da713589a962cf19dad1b6b3115674fdf
Craig R. Hughes <craig.git@rungie.com>
2023-04-26 11:17:40 -0700

    Use updated 1password ssh agent

--- ssh-add.sh
+++ ssh-add.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-/usr/bin/ssh-add -l | /usr/bin/grep -q 'The agent has no identities' && /usr/bin/ssh-add --apple-load-keychain
+_1PASSWORD_SSH_AGENT="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+if [ -S "${_1PASSWORD_SSH_AGENT}" ]; then
+    export SSH_AUTH_SOCK="${_1PASSWORD_SSH_AGENT}"
+else
+    /usr/bin/ssh-add -l | /usr/bin/grep -q 'The agent has no identities' && /usr/bin/ssh-add --apple-load-keychain
+fi
