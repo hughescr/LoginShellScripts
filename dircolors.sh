3e2db696ba4768e4fe90b51a540db5f4c13ac755
Craig R. Hughes <craig.git@rungie.com>
2023-07-07 17:50:41 -0700

    Fix potential escaping problem

--- dircolors.sh
+++ dircolors.sh
@@ -1,5 +1,5 @@
 if [ -x /usr/local/bin/vivid ] || [ -x /opt/homebrew/bin/vivid ]; then
     export LS_COLORS="$(vivid generate snazzy)"
 else
-    dircolors 2>&1 > /dev/null && eval `dircolors "$HOME/.dircolors"`
+    dircolors > /dev/null 2>&1 && eval "$(dircolors "$HOME/.dircolors")"
 fi
