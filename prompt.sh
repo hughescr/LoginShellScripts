85346a5009ebbe423a893de35eaafaf058de3c5e
Craig R. Hughes <craig.git@rungie.com>
2024-12-09 13:02:42 -0800

    Fix prompt color escaping for ZSH per ChatGPT

--- prompt.sh
+++ prompt.sh
@@ -1,18 +1,32 @@
 set -o emacs
 
-# Terminal colours - must use \[ and \] to tell readline about them, so bash line editing works
-inverse="\[$(tput smso)\]"
-uninverse="\[$(tput rmso)\]"
-bold="\[$(tput bold)\]"
-unbold="\[$(tput dim)\]"
-reset="\[$(tput sgr0)\]"
-black="\[$(tput setaf 0)\]"
-red="\[$(tput setaf 1)\]"
-green="\[$(tput setaf 2)\]"
-blue="\[$(tput setaf 4)\]"
-magenta="\[$(tput setaf 5)\]"
-yellow="\[$(tput setaf 3)\]"
-cyan="\[$(tput setaf 6)\]"
+# Terminal colours - must use %{ and %} (zsh) or \[ and \] (bash) to tell readline about them, so zsh line editing works
+if [ -n "$ZSH_VERSION" ]; then
+    # Zsh: Use %{ and %} to enclose non-printing sequences
+    npr_start='%{'
+    npr_end='%}'
+elif [ -n "$BASH_VERSION" ]; then
+    # Bash: Use \[ and \] to enclose non-printing sequences
+    npr_start='\['
+    npr_end='\]'
+else
+    # Default to Bash-style if the shell is not detected
+    npr_start='\['
+    npr_end='\]'
+fi
+
+inverse="${npr_start}$(tput smso)${npr_end}"
+uninverse="${npr_start}$(tput rmso)${npr_end}"
+bold="${npr_start}$(tput bold)${npr_end}"
+unbold="${npr_start}$(tput dim)${npr_end}"
+reset="${npr_start}$(tput sgr0)${npr_end}"
+black="${npr_start}$(tput setaf 0)${npr_end}"
+red="${npr_start}$(tput setaf 1)${npr_end}"
+green="${npr_start}$(tput setaf 2)${npr_end}"
+blue="${npr_start}$(tput setaf 4)${npr_end}"
+magenta="${npr_start}$(tput setaf 5)${npr_end}"
+yellow="${npr_start}$(tput setaf 3)${npr_end}"
+cyan="${npr_start}$(tput setaf 6)${npr_end}"
 
 function _git_prompt() {
     local git_status
@@ -49,3 +63,4 @@ function _prompt_command() {
 }
 
 PROMPT_COMMAND="_prompt_command;$PROMPT_COMMAND"
+
