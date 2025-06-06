2f8bd2dc60951ae07246527cc2a5141ca4d60068
Craig R. Hughes <craig.git@rungie.com>
2025-06-06 14:17:44 +0100

    Add quotes around path which might contain spaces

--- 49_bash_completion.sh
+++ 49_bash_completion.sh
@@ -2,6 +2,6 @@ if [ -f /opt/local/etc/bash_completion ]; then
     . /opt/local/etc/bash_completion
 fi
 
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-    . $(brew --prefix)/etc/bash_completion
+if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
+    . "$(brew --prefix)/etc/bash_completion"
 fi
