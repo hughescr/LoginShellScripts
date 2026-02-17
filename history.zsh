a78f8e1637e0e61cd95d7cd872f8e9e6af04f307
Craig R. Hughes <craig.git@rungie.com>
2026-02-17 11:39:23 -0800

    Better history - all shells write immediately but the do not READ so each term keeps its own separate history in memory but everything is preserved to disk

--- history.zsh
+++ history.zsh
@@ -1,9 +1,13 @@
 export HISTSIZE=500000
 export SAVEHIST=$HISTSIZE
+
 setopt extended_history
-setopt share_history
+setopt incappendhistory
+unsetopt sharehistory
+
 setopt hist_fcntl_lock
 setopt hist_ignore_space
 setopt hist_reduce_blanks
 setopt hist_ignore_all_dups
+
 hgrep () { fc -Dlim "*$@*" 1 }
