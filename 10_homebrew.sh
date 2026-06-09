958c50b8a6fd49e9cf563bb1c35df42eafaf0e94
Craig R. Hughes <craig.git@rungie.com>
2026-06-09 10:23:21 -0700

    Annoying new default "are you sure you want to do the command you just typed" needs to be shot in the head

--- 10_homebrew.sh
+++ 10_homebrew.sh
@@ -5,3 +5,5 @@ elif [ -x /usr/local/bin/brew ]; then
 fi
 
 alias newbrew='(for TAP in $(find ${HOMEBREW_PREFIX}/Library/Taps -type d -name .git); do git --git-dir="${TAP}" changes --no-merges --diff-filter=A --since="2 weeks ago"; done) | fgrep new | cut -d" " -f 5-6 | align -jt'
+
+export HOMEBREW_NO_ASK=1
